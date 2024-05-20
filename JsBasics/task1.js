cf4a5c57e3c7186561f825fc6724f4598e0865f5
Nikita <nikita@skyhive.io>
2024-05-21 00:02:10 +0300

    js_task_one_fix2

--- JsBasics/task1.js
+++ JsBasics/task1.js
@@ -1,12 +1,11 @@
 //Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних: рядок, число, буль, null та undefined. Присвойте їм значення та виведіть кожну змінну на консоль.
 
 
-let userName;
-userName = "Мах";
+
+let userName = "Мах";
 console.log ("User Name;", userName);
 
-let userAge;
-userAge = 33;
+let userAge = 33;
 console.log ("Age:", userAge);
 
 let genderMale = true;
