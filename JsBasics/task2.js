cb9a94728e7aad2a87a54656a6a6927e6e7d5fd9
Nikita <nikita@skyhive.io>
2024-05-22 01:33:28 +0300

    js_task2_fix

--- JsBasics/task2.js
+++ JsBasics/task2.js
@@ -2,8 +2,8 @@
 let UserName1 = "Leonid";
 let UserName2 = "Medved";
 let Slogan = `${UserName1} - drink, ${UserName2} - smoke`;
-console.log (Slogan);
+console.log(Slogan);
 
 //v2 // Використання шаблонного рядка для створення вітання
 let Slogan2 = UserName1 + " - drink, " + UserName2 + " - smoke";
-console.log (Slogan2);
+console.log(Slogan2);
