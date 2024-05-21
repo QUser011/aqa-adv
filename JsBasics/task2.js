c376a7fecd234d45199ff97d2b9e944ac7f312ea
Nikita <nikita@skyhive.io>
2024-05-22 01:15:30 +0300

    js_task2

---
+++ JsBasics/task2.js
@@ -0,0 +1,9 @@
+//v1 // Використання конкатенації рядків
+let UserName1 = "Leonid";
+let UserName2 = "Medved";
+let Slogan = `${UserName1} - drink, ${UserName2} - smoke`;
+console.log (Slogan);
+
+//v2 // Використання шаблонного рядка для створення вітання
+let Slogan2 = UserName1 + " - drink, " + UserName2 + " - smoke";
+console.log (Slogan2);
